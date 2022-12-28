de740fa7e590d232546109d79024a3e190cb22c0
suhedapehlivan <suheda.90@gmail.com>
2022-12-28 13:06:30 +0300

    utility sleep

---
+++ src/utilities/Sleep.java
@@ -0,0 +1,14 @@
+package utilities;
+
+public class Sleep {
+    public static void sleep(double seconds)  {
+
+
+        try {
+            Thread.sleep( (long) (seconds * 1000) );
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}

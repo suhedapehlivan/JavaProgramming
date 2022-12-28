e0b7d70f02e084ed00a3a6cdf8682bf368ab55e3
suhedapehlivan <suheda.90@gmail.com>
2022-12-28 14:37:19 +0300

    java day42

---
+++ src/day42_Exceptions/ThrowsKeyword2.java
@@ -0,0 +1,19 @@
+package day42_Exceptions;
+
+import java.util.NoSuchElementException;
+
+public class ThrowsKeyword2 {
+
+    public static void main(String[] args) throws ArithmeticException {
+
+        System.out.println("Test started");
+
+        System.out.println(8/0);
+
+        System.out.println("Test completed");
+
+
+    }
+
+
+}

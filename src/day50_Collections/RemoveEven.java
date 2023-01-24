399e9b0a1fff45c76675bea9a38ebb7c2f189dd7
suhedapehlivan <suheda.90@gmail.com>
2023-01-24 16:06:21 +0300

    java day50 collections, iterable

--- src/day50_Collections/RemoveEven.java
+++ src/day50_Collections/RemoveEven.java
@@ -9,7 +9,7 @@ import java.util.Set;
 public class RemoveEven {
 
     public static void main(String[] args) {
-
+//remove even elements
         Set<Integer> set = new LinkedHashSet<>(Arrays.asList(1,2,3,4,5,6,100, 90, 80, 75, 45, 35, 25, 15));
 
         //  set.removeIf( p -> p %2 ==0);
